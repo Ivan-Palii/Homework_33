44eb44b1a3bc205d5e624456b1eb95e7d9ada8c2
Ivan Palii <vanopaliy@gmail.com>
2023-07-12 22:06:21 +0900

    Browser Default Actions task complete

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+// 1
+const fieldset = document.querySelector('#contents')
+
+fieldset.addEventListener('click', (e) => {
+	if (e.target.closest('a'))
+		if(!confirm("Do you really want to follow the link?"))
+			e.preventDefault()
+})
+
+// 2
+const gallery = document.querySelector('#gallery')
+const largeImg = document.querySelector('#largeImg')
+
+gallery.addEventListener('click', (e) => {
+	if (e.target.closest('a')) {
+		largeImg.src = e.target.closest('a').href
+		e.preventDefault()
+	}
+})
